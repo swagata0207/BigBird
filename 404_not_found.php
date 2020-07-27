9a7decc916f7eaf9a374ce40a4b2543b220fd8a2
swagata0207 <its.swagata.sasmal@gmail.com>
2020-07-27 23:27:17 +0530

    error404 page

---
+++ 404_not_found.php
@@ -0,0 +1,30 @@
+<!--<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title></title>
+		<link rel="stylesheet" href="pagestyles.css">
+		<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap" rel="stylesheet">
+	</head>
+	<body>
+		<h1>We haven't named our website yet</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Link1</a></li>
+				<li><a href="#">Link2</a></li>
+				<li><a href="#">Link3</a></li>
+				<li><a href="#">Link4</a></li>
+				<li class="right"><a href="#">Linkn</a></li>
+				<li class="right"><a href="#">Linkn-1</a></li>
+			</ul>
+		</nav>
+		<div>-->
+			<?php
+				header("HTTP/1.0 404 Not Found");
+				echo "Sorry, this page doesn't exist";
+			?>
+<!--		</div>
+	</body>
+</html>-->
